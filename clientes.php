13c02217b61e4fc34681709b0aad5e200ddb2438
cauancalmeida <cauan.c.almeida@gmail.com>
2016-02-15 22:54:44 -0200

    Create clientes.php

---
+++ clientes.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<title>clientes</title>
+</head>
+<body>
+<?php
+include('menu.html');
+include_once ('conecta/conecta.php');
+$query="SELECT * FROM Cliente ORDER BY nome";
+$result= mysql_query($query, $con);
+//$result= mysql_query($con, $query);
+?>
+<div class="container">
+<div class="table-responsive">
+<table class="table">
+<thead>
+	<th>#</th>
+	<th>Nome</th>
+	<th>Telefone</th>
+	<th>Email</th>
+</thead>
+<tbody>
+<?php 
+while(mysql_fetch_row($result)){
+	echo'<tr>';
+	echo'<td>'.mysql_result($result, 'nome').'</td>';
+	echo'<td>'.mysql_result($result, 'telefone').'</td>';
+	echo'<td>'.mysql_result($result, 'email').'</td>';
+	echo '<button type="button" class="btn btn-danger" onclick="window.location.href='.'deletarCliente.php'.'">Deletar</button>';
+}
+
+?>
+</tr>
+</tbody>
+</table>
+</div>
+</div>
